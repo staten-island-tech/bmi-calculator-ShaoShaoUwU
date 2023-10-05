2539b83b95b18e25429a800ed172b0a000fca50d
Shao <leo21604212@gmail.com>
2023-10-05 13:00:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-function calculateBMI() {
-    const weight = Number(prompt("Enter your weight in kilograms:"));
-    const height = Number(prompt("Enter your height in meters:"));
+const weight = Number(prompt("Enter your weight in kilograms:"));
+const height = Number(prompt("Enter your height in meters:"));
 
+
+function calculateBMI() {
     const bmi = weight / (height * height);
     console.log(bmi);
+    return bmi;
+    
 }
 
-calculateBMI();
+function Fatty(){
+    const bmi = calculateBMI();
+    if(bmi < 18.5){
+        console.log("Underweight");
+
+    }
+    else if(bmi > 24.9){
+        console.log("Overweight");
+    
+    }
+    else{
+        console.log("Healthy");
+    }
+}
+Fatty();
